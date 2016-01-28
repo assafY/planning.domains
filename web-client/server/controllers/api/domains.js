348f3747f5def84e996781ad97ab831ebcc12fcd
assafY <assaf.yossifoff@kcl.ac.uk>
2016-01-28 16:30:25 +0000

    minor

--- web-client/server/controllers/api/domains.js
+++ web-client/server/controllers/api/domains.js
@@ -4,19 +4,6 @@ var router = require('express').Router();
 var request = require('request');
 var xmlParser = require('xml2js').parseString;
 
-/*var domains = [
-	{
-		name: 'Depots',
-		formulation: 'Temporal',
-		ipc: '2002'
-	},
-	{
-		name: 'Driverlog',
-		formulation: 'Timewindows',
-		ipc: '2004'
-	}
-]*/
-
 router.get('/', function(req, res) {
 	
 	request('http://calcium.inf.kcl.ac.uk:8080/', function(error, response, body) {
@@ -26,7 +13,6 @@ router.get('/', function(req, res) {
 			});
 		}
 	});
-	//res.json(domains);
 });
 
 router.post('/', function(req, res) {
