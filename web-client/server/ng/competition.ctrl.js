05e287c1234f3470738012370a94daa17f0f33f9
assafY <assaf.yossifoff@kcl.ac.uk>
2016-02-13 17:50:07 +0000

    added angular competition controller and service

--- web-client/server/ng/competition.ctrl.js
+++ web-client/server/ng/competition.ctrl.js
@@ -1,4 +1,12 @@
 angular.module('app')
-.controller('CompetitionController', function($scope) {
-	
-})
+.controller('CompetitionController', function($rootScope, $scope) {
+	if (!$rootScope.leaderboard) {
+        CompetitionService.fetchLeaderboard().success (function (leaderboard) {
+            if (leaderboard) {
+                $scope.leaderboard = $rootScope.leaderboard = leaderboard
+            }
+        })
+    } else {
+        $scope.leaderboard = $rootScope.leaderboard
+    }
+});
