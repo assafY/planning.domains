04ae43e180f4d085161638fcb47316141f1bd406
assafY <assaf.yossifoff@kcl.ac.uk>
2016-02-28 17:34:08 +0000

    got css and js to load and reload on view change

--- web-client/server/ng/module.js
+++ web-client/server/ng/module.js
@@ -1,3 +1,9 @@
 angular.module('app', [
 	'ngRoute'
-])
+]).run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$viewContentLoaded', function() {
+        $timeout(function() {
+            componentHandler.upgradeAllRegistered();
+        });
+    });
+});
